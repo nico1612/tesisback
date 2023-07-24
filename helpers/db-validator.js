9c9c4f4be5506cc5d025392affd0e977eb30c747
nico1612 <nicogonzalezraggi123@hotmail.com>
2023-07-24 12:45:53 -0300

    limpieza de codigo

--- helpers/db-validator.js
+++ helpers/db-validator.js
@@ -1,7 +1,6 @@
 import { Role } from "../models/role.js";
 import { Usuario } from "../models/usuario.js";
 
-
 export const esRoleValido = async(rol = '') => {
 
     const existeRol = await Role.findOne({ rol });
