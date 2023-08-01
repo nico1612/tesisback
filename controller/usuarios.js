bb5e560a876bfc0d9b8e27034b3b67b2ba4fb688
nicolas gonzalez raggi <nicolasgonzalezraggi@MBP-de-nicolas-12.fibertel.com.ar>
2023-08-01 00:00:16 -0300

    cambio en usuarios

--- controller/usuarios.js
+++ controller/usuarios.js
@@ -4,22 +4,10 @@ import bcryptjs from 'bcryptjs';
 import { Usuario } from "../models/usuario.js";
 
 export const usuariosGet = async(req = request, res = response) => {
-
-    const { limite = 5, desde = 0 } = req.query;
-    const query = { estado: true };
-
-    const [ total, usuarios ] = await Promise.all([
-        Usuario.countDocuments(query),
-        Usuario.find(query)
-            .skip( Number( desde ) )
-            .limit(Number( limite ))
-    ]);
-
+    const usuario = await Usuario.find();
     res.json({
-        total,
-        usuarios
+        usuario,
     });
-
 }
 
 export const usuariosPost = async(req, res = response) => {
